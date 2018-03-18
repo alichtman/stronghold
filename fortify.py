e16339fffe8fe08de9bac4ca00c13ff98a6b9aca
alichtman <aaronlichtman@gmail.com>
2018-03-18 15:42:25 -0500

    Added coloring of prompts and headers and splash screen

--- fortify.py
+++ fortify.py
@@ -10,9 +10,9 @@
 #
 # Maybe turn this into a wrapper for: https://github.com/kristovatlas/osx-config-check/blob/master/app.py
 
-import subprocess as sp
 import sys
-
+import subprocess as sp
+from colorama import init, Fore, Style
 
 def prompt_yes_no(question):
 	"""Print question and return True or False. Thanks, @shobrook"""
@@ -20,20 +20,21 @@ def prompt_yes_no(question):
 	prompt = " [Y/n] "
 
 	while True:
-		print(question + prompt)
+		print(Fore.GREEN + Style.BRIGHT + question + prompt)
+		print(Style.RESET_ALL)
 		choice = input().lower()
 		if choice in valid:
 			return valid[choice]
 		else:
-			print("Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
+			print(Fore.RED + Style.BRIGHT + "Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
 
 
-def print_section_header(title):
-	print("\n########\n" + "# " + title + "\n########\n")
+def print_section_header(title, color):
+	print(color + Style.BRIGHT + "\n########\n" + "# " + title + "\n########\n")
 
 
 def splash_intro():
-	print(
+	print(Fore.GREEN +
 		"\n88888888888                                       ad88 \n" +
 		"88                                  ,d     **    d8` \n" +
 		"88                                  88           88 \n" +
@@ -43,18 +44,15 @@ def splash_intro():
 		"88        8a,   ,a8   88            88,    88    88        8b,d8 \n" +
 		"88         `YbbdP`    88            \Y888  88    88         Y88 \n" +
 		"                                                            d8 \n" +
-		"                                                           d8 \n")
+		"                                                           d8 \n" + Style.RESET_ALL)
 
-	print("Fortify is a security configuration tool for MacOS Sierra and High Sierra.")
-	print("You may be asked for the sudo password.")
+	print(Fore.BLUE + Style.BRIGHT + "Fortify is a security configuration tool for MacOS Sierra and High Sierra.")
+	print("You may be asked for the sudo password." + Style.RESET_ALL)
 
-	print_section_header("BEFORE RUNNING")
+	print_section_header("WARNINGS", Fore.RED)
 
-	print("Ensure you have up-to-date backups.")
-
-	print_section_header("WARNINGS")
-
-	print("\t1. This script modifies system settings. There is always the possibility it will damage your system.")
+	print("\t0. Ensure you have up-to-date backups.")
+	print("\t1. This script modifies system settings. There is always the possibility it may damage your system.")
 	print("\t2. Do not key-mash through this script. Things you do not want to happen might happen.\n")
 
 	if not prompt_yes_no("I have read the above and want to continue."):
@@ -67,28 +65,28 @@ def splash_intro():
 def firewall_config():
 	if prompt_yes_no("Turn on firewall?"):
 
-		print("Enabling firewall...")
+		print(Fore.YELLOW + Style.DIM + "Enabling firewall..." + Style.RESET_ALL)
 		sp.run(['sudo', '/usr/libexec/ApplicationFirewall/socketfilterfw', '--setglobalstate', 'on'], stdout=sp.PIPE)
 
-		if prompt_yes_no("\n-> Turn on logging?"):
-			print("Enabling logging...")
+		if prompt_yes_no("-> Turn on logging?"):
+			print(Fore.YELLOW + Style.DIM + "Enabling logging..." + Style.RESET_ALL)
 			sp.run(['sudo', '/usr/libexec/ApplicationFirewall/socketfilterfw', '--setloggingmode', 'on'], stdout=sp.PIPE)
 
-		if prompt_yes_no("\n-> Turn on stealth mode?"):
-			print("Enabling stealth mode...")
+		if prompt_yes_no("-> Turn on stealth mode?"):
+			print(Fore.YELLOW + Style.DIM + "Enabling stealth mode..." + Style.RESET_ALL)
 			sp.run(['sudo', '/usr/libexec/ApplicationFirewall/socketfilterfw', '--setstealthmode', 'on'], stdout=sp.PIPE)
 
-		if prompt_yes_no("\n-> Prevent software from being whitelisted automatically?"):
-			print("Preventing automatic whitelisting...")
+		if prompt_yes_no("-> Prevent software from being whitelisted automatically?"):
+			print(Fore.YELLOW + Style.DIM + "Preventing automatic whitelisting..." + Style.RESET_ALL)
 			sp.run(['sudo', '/usr/libexec/ApplicationFirewall/socketfilterfw', '--setallowsigned', 'off'], stdout=sp.PIPE)
 			sp.run(['sudo', '/usr/libexec/ApplicationFirewall/socketfilterfw', '--setallowsignedapp', 'off'], stdout=sp.PIPE)
 
-		print("Resetting firewall to finalize changes...")
+		print(Fore.YELLOW + Style.DIM + "Resetting firewall to finalize changes..." + Style.RESET_ALL)
 		sp.run(['sudo', 'pkill', '-HUP', 'socketfilterfw'], stdout=sp.PIPE)
 
 def captive_portal_config():
 	if prompt_yes_no("Disable Captive Portal Assistant and force login through browser?"):
-		print("Disabling Captive Portal Assistant...")
+		print(Fore.YELLOW + Style.DIM + "Disabling Captive Portal Assistant..." + Style.RESET_ALL)
 		sp.run(['sudo', 'defaults', 'write', '/Library/Preferences/SystemConfiguration/com.apple.captive.control', 'Active', '-bool', 'false'], stdout=sp.PIPE)
 
 
@@ -125,34 +123,36 @@ def metadata_storage_config():
 
 def general_safety_config():
 	if prompt_yes_no("Lock Mac as soon as screen saver starts?"):
-		print("Configuring...")
+		print(Fore.YELLOW + Style.DIM + "Configuring..." + Style.RESET_ALL)
 		sp.run(['defaults', 'write', 'com.apple.screensaver', 'askForPassword', '-int', '1'], stdout=sp.PIPE)
 		sp.run(['defaults', 'write', 'com.apple.screensaver', 'askForPasswordDelay', '-int', '0'], stdout=sp.PIPE)
 
-	if prompt_yes_no("\nDisplay all file extensions?"):
-		print("Configuring...")
+	if prompt_yes_no("Display all file extensions?"):
+		print(Fore.YELLOW + Style.DIM + "Configuring..." + Style.RESET_ALL)
 		sp.run(['defaults', 'write', 'NSGlobalDomain', 'AppleShowAllExtensions', '-bool', 'true'], stdout=sp.PIPE)
-	if prompt_yes_no("\nDisable saving to the cloud by default?"):
-		print("Configuring...")
+
+	if prompt_yes_no("Disable saving to the cloud by default?"):
+		print(Fore.YELLOW + Style.DIM + "Configuring..." + Style.RESET_ALL)
 		sp.run(['defaults', 'write', 'NSGlobalDomain', 'NSDocumentSaveNewDocumentsToCloud', '-bool', 'false'], stdout=sp.PIPE)
-	if prompt_yes_no("\nShow hidden files in Finder?"):
-		print("Configuring...")
+
+	if prompt_yes_no("Show hidden files in Finder?"):
+		print(Fore.YELLOW + Style.DIM + "Configuring..." + Style.RESET_ALL)
 		sp.run(['defaults', 'write', 'com.apple.finder', 'AppleShowAllFiles', '-boolean', 'true'], stdout=sp.PIPE)
 
 
 if __name__ == '__main__':
 	splash_intro()
 
-	print_section_header("FIREWALL")
+	print_section_header("FIREWALL", Fore.BLUE)
 	firewall_config()
 
-	print_section_header("CAPTIVE PORTAL")
+	print_section_header("CAPTIVE PORTAL", Fore.BLUE)
 	captive_portal_config()
 
-	# print_section_header("USER DATA COLLECTION")
+	# print_section_header("USER DATA COLLECTION", Fore.BLUE)
 	# metadata_storage_config()
 
-	print_section_header("GENERAL SAFETY")
+	print_section_header("GENERAL SAFETY", Fore.BLUE)
 	general_safety_config()
 
-	print("\nConfiguration complete!")
+	print(Fore.YELLOW + Style.BRIGHT + "\nConfiguration complete!", )
