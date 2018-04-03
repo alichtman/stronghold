42bdef64c1a5a56d839a786f4a9a5341510d3630
alichtman <aaronlichtman@gmail.com>
2018-04-02 20:05:39 -0500

    Removed fortify.py and updated repo structure

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
-import _version
+import constants
 
 here = path.abspath(path.dirname(__file__))
 
