80808b5602350eb9154315a31d198e940095805f
alichtman <aaronlichtman@gmail.com>
2018-04-06 19:22:59 -0500

    New version

--- constants.py
+++ constants.py
@@ -1,9 +1,8 @@
 class Constants:
 	PROJECT_NAME='stronghold'
-	VERSION='1.1.1'
+	VERSION='1.1.2'
 	AUTHOR_GITHUB='alichtman'
 	AUTHOR_FULL_NAME='Aaron Lichtman'
-	PUBLISHED="04/02/2018"
 	DESCRIPTION="Securely configure your Mac from the terminal."
 	URL='https://github.com/alichtman/stronghold',
 	AUTHOR_EMAIL='aaronlichtman@gmail.com',
