6b481dba8f3f0de668eac0be45f339cf40d488b9
alichtman <aaronlichtman@gmail.com>
2018-05-26 04:02:30 -0500

    Add Disable Printer Sharing and restructure final_config

--- stronghold.py
+++ stronghold.py
@@ -239,6 +239,12 @@ def user_safety_config():
 		print_confirmation("Displaying hidden files in Finder...")
 		sp.run(['defaults', 'write', 'com.apple.finder', 'AppleShowAllFiles', '-boolean', 'true'], shell=True, stdout=sp.PIPE)
 
+	if prompt_yes_no(top_line="-> Disable printer sharing?",
+	                 bottom_line="Offers redundancy in case the Firewall was not configured."):
+		print_confirmation("Disabling printer sharing...")
+		sp.run(['cupsctl', '--no-share-printers'], shell=True, stdout=sp.PIPE)
+
+
 	# Reset finder after messing with it.
 	print_confirmation("Resetting Finder to finalize changes...")
 	sp.run(['killAll', 'Finder'], stdout=sp.PIPE)
@@ -247,8 +253,12 @@ def user_safety_config():
 def final_configuration():
 	print_section_header("FINAL CONFIGURATION STEPS", Fore.BLUE)
 
-	print(Fore.RED + Style.BRIGHT + "WARNING: Configuration not complete! A full restart is necessary." + Style.RESET_ALL)
-	print(Fore.YELLOW + Style.BRIGHT + "If you have any suggestions for stronghold, open an issue at: https://github.com/alichtman/stronghold/issues/new" + Style.RESET_ALL)
+	print(Fore.RED + Style.BRIGHT + "WARNING: Secure Configuration not complete! At a minimum, a full restart is necessary." + Style.RESET_ALL)
+	print(Fore.BLUE + Style.BRIGHT + "\nNEXT STEPS...\n" + Style.RESET_ALL)
+	print(Fore.BLUE + Style.BRIGHT + "1. Fully disable Siri: https://apple.stackexchange.com/a/258981" + Style.RESET_ALL)
+	print(Fore.BLUE + Style.BRIGHT + "2. Enable FileVault. (Check if it's enabled by running `$ fdesetup status`)" + Style.RESET_ALL)
+
+	print(Fore.YELLOW + Style.BRIGHT + "\nIf you have any suggestions for stronghold, open an issue at: https://github.com/alichtman/stronghold/issues/new" + Style.RESET_ALL)
 	sys.exit()
 
 	# Removed due to bug with sudo reboot.
